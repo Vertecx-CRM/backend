e00edbd65e19da82c0714d4395a122f800bebec4
Scorhenao <samcorhenao@gmail.com>
2025-10-19 17:03:18 -0500

    fix rule violations

--- vertecx-api/src/config/database.config.ts
+++ vertecx-api/src/config/database.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10) || 17345,
+  port: parseInt(process.env.DB_PORT, 10),
   user: process.env.DB_USER,
   pass: process.env.DB_PASS,
   name: process.env.DB_NAME,
