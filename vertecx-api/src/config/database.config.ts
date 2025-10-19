482a223ee7d8858994b451c19117ebc17c28b7ad
Scorhenao <samcorhenao@gmail.com>
2025-10-19 17:09:11 -0500

    use .env

--- vertecx-api/src/config/database.config.ts
+++ vertecx-api/src/config/database.config.ts
@@ -2,7 +2,11 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   host: process.env.DB_HOST,
+<<<<<<< HEAD
   port: parseInt(process.env.DB_PORT, 10),
+=======
+  port: parseInt(process.env.DB_PORT, 10) || 17345,
+>>>>>>> 709dfc2 (Db conecction and script to the entities)
   user: process.env.DB_USER,
   pass: process.env.DB_PASS,
   name: process.env.DB_NAME,
