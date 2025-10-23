43b4c64641f72f2ecc002da7c56bbc1e2b6a5d62
JoaoOrtz <joaoestid@gmail.com>
2025-10-23 15:27:33 -0500

    `Deleted frontend project files and dependencies`

--- src/appoiments/dto/update-appoiment.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAppoimentDto } from './create-appoiment.dto';
-
-export class UpdateAppoimentDto extends PartialType(CreateAppoimentDto) {}
--- src/technicians/technicians.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TechniciansService } from './technicians.service';
-import { TechniciansController } from './technicians.controller';
-
-@Module({
-  controllers: [TechniciansController],
-  providers: [TechniciansService],
-})
-export class TechniciansModule {}
