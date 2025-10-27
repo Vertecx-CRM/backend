84e50d56b4adc606f082d0c4e4fb173a8f5a5761
Scorhenao <samcorhenao@gmail.com>
2025-10-27 08:46:46 -0500

    Refactor: add another port on the main with .env

--- vertecx-api/src/main.ts
+++ vertecx-api/src/main.ts
@@ -22,7 +22,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(3000);
-  console.log('🚀 API corriendo en http://localhost:3000/api/docs');
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 API corriendo en http://localhost:${port}/api/docs`);
 }
 bootstrap();
