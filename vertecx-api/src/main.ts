ec34a7b4d9e4dd3635721469e1e48b6e9051fdf3
danier00 <danieralvarez090@gmail.com>
2025-11-03 22:26:48 -0500

    Agregue puerto 3000 en cors

--- vertecx-api/src/main.ts
+++ vertecx-api/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3001',
+      'http://localhost:3000',
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
