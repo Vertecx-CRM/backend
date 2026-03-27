ac9e9881307c5ce1b949bac1ab90a3ceb35abcae
Scorhenao <samcorhenao@gmail.com>
2026-03-27 10:02:28 -0500

    feat: add backend security headers

--- vertecx-api/src/main.ts
+++ vertecx-api/src/main.ts
@@ -6,6 +6,19 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  expressApp.disable('x-powered-by');
+  expressApp.use((req, res, next) => {
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+    res.setHeader(
+      'Strict-Transport-Security',
+      'max-age=31536000; includeSubDomains',
+    );
+    next();
+  });
 
   app.enableCors({
     origin: (origin, callback) => callback(null, true),
